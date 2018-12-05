7a2e54ef219a082a0ff59be8d6b20e69e169aa27
Haaris Memon <memonhaaris97@gmail.com>
2018-12-05 18:22:09 +0000

    Add a click listener to an argument preview

--- client/src/components/ArgumentPreview.jsx
+++ client/src/components/ArgumentPreview.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ArgumentPreview = (props) => (
-  <div className="ArgumentPreview">
-    <div className="scheme-name">
-      Scheme: {props.scheme}
-    </div>
-    <div className="value-name">
-      <div>
-        Circumstance: {props.circumstance}
+class ArgumentPreview extends React.Component {
+  handleClick = () => {
+    console.log(this.props.action)
+  }
+
+  render() {
+    return (
+      <div className="ArgumentPreview" onClick={this.handleClick}>
+        <div className="scheme-name">
+          Scheme: {this.props.scheme}
+        </div>
+        <div className="value-name">
+          <div>
+            Circumstance: {this.props.circumstance}
+          </div>
+          <div>
+            Action: {this.props.action}
+          </div>
+          <div>
+            Goal: {this.props.goal}
+          </div>
+          <div>
+            Value: {this.props.value}
+          </div>
+        </div>
       </div>
-      <div>
-        Action: {props.action}
-      </div>
-      <div>
-        Goal: {props.goal}
-      </div>
-      <div>
-        Value: {props.value}
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 ArgumentPreview.propTypes = {
   scheme: PropTypes.string.isRequired,
