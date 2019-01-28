2b0b9b184381987f04e6e95ab6803e3a9a189fbc
Haaris Memon <memonhaaris97@gmail.com>
2019-01-28 16:20:53 +0000

    Add a vis.js network to the argument details page

--- client/src/components/pages/ArgumentDetails.js
+++ client/src/components/pages/ArgumentDetails.js
@@ -1,17 +1,71 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Graph from 'vis-react'
 
 import Header from '../Header'
 import ArgumentNest from '../ArgumentNest'
 
-const ArgumentDetails = (props) => {
-  return (
-    <div>
-      <Header title="Argument" />
-      <Link className="GoBackHomeLink" to="/">Go back home</Link>
-      <ArgumentNest level={0} rootId={props.match.params.id}/>
-    </div>
-  )
+class ArgumentDetails extends React.Component {
+  state = {
+    showNetwork: false,
+    networkToggleText: 'Show Argument Network'
+  }
+
+  handleNetworkToggle() {
+    var newToggleText = null;
+
+    if(this.state.showNetwork) {
+      newToggleText = 'Show Argument Network'
+    } else {
+      newToggleText = 'Show Nested Arguments'
+    }
+
+    this.setState({
+      showNetwork: !this.state.showNetwork,
+      networkToggleText: newToggleText
+    })
+  }
+
+  render() {
+    var graph = {
+      nodes: [
+          {id: 1, label: 'Node 1'},
+          {id: 2, label: 'Node 2'},
+          {id: 3, label: 'Node 3'},
+          {id: 4, label: 'Node 4'},
+          {id: 5, label: 'Node 5'}
+        ],
+      edges: [
+          {from: 1, to: 2},
+          {from: 1, to: 3},
+          {from: 2, to: 4},
+          {from: 2, to: 5}
+        ]
+    };
+
+    var options = {};
+
+    var events = {
+        select: function(event) {
+            var { nodes, edges } = event;
+        }
+    }
+
+    return (
+      <div>
+        <Header title="Argument" />
+        <Link className="GoBackHomeLink" to="/">Go back home</Link>
+        <button id="network-toggle" onClick={this.handleNetworkToggle.bind(this)}>{this.state.networkToggleText}</button>
+        {this.state.showNetwork ?
+          <div id="argument-network">
+            <Graph graph={graph} options={options} events={events} />
+          </div>
+          :
+          <ArgumentNest level={0} rootId={this.props.match.params.id}/>
+        }
+      </div>
+    )
+  }
 }
 
 export default ArgumentDetails
