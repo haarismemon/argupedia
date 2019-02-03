1797989b7a3db3c13642a03fc101ab20e4a4b179
Haaris Memon <memonhaaris97@gmail.com>
2019-02-03 11:56:55 +0000

    Implement session handling for the components

--- client/src/App.js
+++ client/src/App.js
@@ -17,27 +17,26 @@ import ForgotPassword from './components/Pages/ForgotPassword'
 import Account from './components/Pages/Account'
 import Error from './components/Pages/Error'
 
+import { withAuthentication } from './components/Session/index'
 import * as ROUTES from './constants/routes'
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Navigation />
-        <hr/>
-        <Switch>
-          <Route path={ROUTES.HOME} component={ArgumentList} exact />
-          <Route path={ROUTES.ARGUMENT_DETAILS} component={ArgumentDetails} exact />
-          <Route path={ROUTES.SUBMIT_ARGUMENT} component={SubmitArgument} />
-          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.FORGOT_PASSWORD} component={ForgotPassword} />
-          <Route path={ROUTES.ACCOUNT} component={Account} />
-          <Route component={Error} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Navigation />
+      <hr/>
+      <Switch>
+        <Route path={ROUTES.HOME} component={ArgumentList} exact />
+        <Route path={ROUTES.ARGUMENT_DETAILS} component={ArgumentDetails} exact />
+        <Route path={ROUTES.SUBMIT_ARGUMENT} component={SubmitArgument} />
+        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route path={ROUTES.FORGOT_PASSWORD} component={ForgotPassword} />
+        <Route path={ROUTES.ACCOUNT} component={Account} />
+        <Route component={Error} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+      
+export default withAuthentication(App);
