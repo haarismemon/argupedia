ee0193afc0dd96310c6cc1249b61ff114a7c724f
Haaris Memon <memonhaaris97@gmail.com>
2019-02-11 18:24:07 +0000

    Implement main functionality of labelling algo. for grounded semantics

--- server/src/routes/argument.js
+++ server/src/routes/argument.js
@@ -103,9 +103,117 @@ router.get('/argument/network', (req, res) => {
 
   ArgumentModel.find(query, (err, arguments) => {
     let nodesAndAttacks = generateNodesAndAttacks(arguments);
+
+    const labelledNodes = groundedLabellingAlgorithm(nodesAndAttacks);
+
+    console.log(labelledNodes);
+    
+
     return res.json(nodesAndAttacks)
   })
 })
+
+function groundedLabellingAlgorithm(nodesAndAttacks) {
+  const allNodes = nodesAndAttacks.nodes;
+
+  // let previousLabelling = null;
+  // start of with all labels being empty
+  let currentLabelling = {
+    in: [],
+    out: [],
+    undec: []
+  }
+
+  // TODO: if all arguments are being attacked then all arguments are undecided
+
+  // keep repeating until previous iteration labelling = current iteration labelling
+  let breakLoop = false;
+  while(!breakLoop) {
+    let newLabelling = {
+      in: [],
+      out: [],
+      undec: []
+    }
+
+    const unlabelledArguments = calculateUnlabelledArguments(currentLabelling, allNodes);
+
+    // in(Li+1) = in(Li) ∪ {x | x is not labelled in Li, and ∀y : if yRx then y ∈ out(Li) }
+    // label x IN if it is not labelled, and all arguments that attack x are out
+    unlabelledArguments.forEach(node => {
+      // get all arguments that attack the current node
+      const allAttackingArguments = getAllAttackingArguments(nodesAndAttacks, node);
+      
+      let allAttackingArgumentsAreOut = true;
+      allAttackingArguments.forEach(attackingArgumentId => {
+        if(!currentLabelling.out.some(a => a.id === attackingArgumentId)) {
+          allAttackingArgumentsAreOut = false;
+        }
+      });
+
+      // if all are out, then add node to IN
+      if(allAttackingArgumentsAreOut) {
+        newLabelling.in.push(node);
+      }
+    });
+
+    // out(Li+1) = out(Li) ∪ {x | x is not labelled in Li, and ∃y : yRx and y ∈ in(Li+1) }
+    // label x OUT if it is not labelled, and there is an argument that attacks it that is IN (in current iteration)
+    unlabelledArguments.forEach(node => {
+      // get all arguments that attack the current node
+      const allAttackingArguments = getAllAttackingArguments(nodesAndAttacks, node);
+      
+      let oneAttackingArgumentIsIn = false;    
+      allAttackingArguments.forEach(attackingArgumentId => {
+        if(newLabelling.in.some(a => a.id === attackingArgumentId)) {
+          oneAttackingArgumentIsIn = true;
+        }
+      });
+
+      // if there is one attacking that is IN, then add node to OUT
+      if(oneAttackingArgumentIsIn) {
+        newLabelling.out.push(node);
+      }
+    });
+
+    const newIn = currentLabelling.in.concat(newLabelling.in);
+    const newOut = currentLabelling.out.concat(newLabelling.out);
+
+    if(newIn.length === currentLabelling.in.length && newOut.length == currentLabelling.out.length) {
+      breakLoop = true;
+    }
+    
+    currentLabelling.in = newIn;
+    currentLabelling.out = newOut;
+    // console.log(currentLabelling);
+    
+  }
+  // LG = (in(Li), out(Li), A − (in(Li) ∪ out(Li) )
+  currentLabelling.undec = calculateUnlabelledArguments(currentLabelling, allNodes);
+
+  return currentLabelling;
+
+}
+
+function calculateUnlabelledArguments(currentLabelling, allNodes) {
+  const union = new Set(currentLabelling.in.concat(currentLabelling.out));
+  return allNodes.filter(argument => !union.has(argument));
+}
+
+function getAllAttackingArguments(nodesAndAttacks, node) {
+  let attackingArguments = [];
+
+  nodesAndAttacks.edges.forEach(attack => {
+    const attackingArgument = attack.from;
+    const attackedArgument = attack.to;
+
+    // store the attacking argument, if the argument being attacked matches the id of the provided node
+    if(attackedArgument == node.id) {
+      attackingArguments.push(attackingArgument);
+    }
+  });
+
+  return attackingArguments;
+}
 
 function generateNodesAndAttacks(arguments) {
   let nodesAndAttacks = {
