39513c9b4bd7faca1ca84fa14ccfabcd5d21d87b
Haaris Memon <memonhaaris97@gmail.com>
2019-02-15 15:44:39 +0000

    Implement own version of setting the active links in the navbar

--- client/src/components/Navigation/NavigationBar.js
+++ client/src/components/Navigation/NavigationBar.js
@@ -3,55 +3,62 @@ import {LinkContainer} from 'react-router-bootstrap'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavLink from 'react-bootstrap/NavLink';
+import { withRouter } from 'react-router-dom'
 
 import * as ROUTES from '../../constants/routes'
 import SignOutButton from './SignOutButton'
 import { AuthUserContext } from '../Session';
 
-const NavigationBar = () => {
-    return (
-        <Navbar bg="dark" variant="dark" expand="sm">
-            <LinkContainer to={ROUTES.HOME}>
-                <Navbar.Brand>Debatably</Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <LinkContainer to={ROUTES.HOME} exact>
-                        <NavLink>Home</NavLink>
-                    </LinkContainer>
-                    <LinkContainer to={ROUTES.SUBMIT_ARGUMENT}>
-                        <NavLink>Submit Argument</NavLink>
-                    </LinkContainer>
-                    <AuthUserContext.Consumer>
-                        { authUser => 
-                            authUser ? <NavigationAuth /> : <NavigationNonAuth />
-                        }
-                    </AuthUserContext.Consumer>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
-};
+import './Navigation.css';
 
-const NavigationAuth = () => (
+class NavigationBar extends React.Component {
+    render() {
+        const currentPath = this.props.history.location.pathname;
+        
+        return (
+            <Navbar bg="dark" variant="dark" expand="sm" onSelect={this.handleClick}>
+                <LinkContainer to={ROUTES.HOME}>
+                    <Navbar.Brand>Debatably</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <ActiveLink to={ROUTES.HOME} currentPath={currentPath} text="Home"/>
+                        <ActiveLink to={ROUTES.SUBMIT_ARGUMENT} currentPath={currentPath} text="Submit Argument"/>
+                        <AuthUserContext.Consumer>
+                            { authUser => 
+                                authUser ? <NavigationAuth currentPath={currentPath}/> : <NavigationNonAuth currentPath={currentPath}/>
+                            }
+                        </AuthUserContext.Consumer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
+
+const NavigationAuth = (props) => (
     <div className="navbar-right">
-        <LinkContainer to={ROUTES.ACCOUNT}>
-            <NavLink>Account</NavLink>
-        </LinkContainer>
+        <ActiveLink to={ROUTES.ACCOUNT} currentPath={props.currentPath} text="Account"/>
         <SignOutButton />
     </div>
 )
 
-const NavigationNonAuth = () => (
+const NavigationNonAuth = (props) => (
     <div className="navbar-right">
-        <LinkContainer to={ROUTES.SIGN_IN}>
-            <NavLink>Sign In</NavLink>
-        </LinkContainer>
-        <LinkContainer to={ROUTES.SIGN_UP}>
-            <NavLink>Sign Up</NavLink>
-        </LinkContainer>
+        <ActiveLink to={ROUTES.SIGN_IN} currentPath={props.currentPath} text="Sign In"/>
+        <ActiveLink to={ROUTES.SIGN_UP} currentPath={props.currentPath} text="Sign Up"/>
     </div>
 )
 
-export default NavigationBar
+const ActiveLink = (props) => {
+    return (
+        <LinkContainer to={props.to} exact>
+            <NavLink className={props.currentPath === props.to ? "active-link" : ""}>
+                {props.text}
+            </NavLink>
+        </LinkContainer>
+    )
+}
+
+export default withRouter(NavigationBar)
